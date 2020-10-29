223d22f214cddd5b956f9d4633acff3b8833f244
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-10-29 13:40:05 -0500

    datos de usuario y se organiza problema en el dropdown

--- src/components/StartApp.jsx
+++ src/components/StartApp.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 import { Accordion, Card, Col, Row } from "react-bootstrap";
+import faker from 'faker'
+import { API_MEDIATEC_APP } from '../constantes'
 
 export default class StartApp extends Component {
     constructor(props) {
         super()
-        // props.App.setState({ menu_option: "home" })
+        this.state = {
+            modulos: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(`${API_MEDIATEC_APP}modulos`)
+            .then(respuesta => respuesta.json())
+            .then(modulos => this.setState({ modulos: modulos }))
     }
 
     render() {
@@ -23,68 +33,25 @@ export default class StartApp extends Component {
                             </Card>
                         </Col>
                     </Row>
+
                     <Row>
-                        <Col lg={6} className="mb-5">
-                            <Accordion defaultActiveKey="0">
-                                <Card bg="primary">
-                                    <Accordion.Toggle as={Card.Header} eventKey="0" role="button" aria-expanded="true" className="btn btn-primary py-2 shadow-sm with-chevron">
-                                        <p className="d-flex align-items-center justify-content-between mb-0 px-3 py-2"><strong className="text-uppercase">Construcción de Elementos de Software 1</strong><i className="fa fa-angle-down"></i></p>
-                                    </Accordion.Toggle>
-                                    <Accordion.Collapse eventKey="0" className="shadow-sm">
-                                        <Card.Body>
-                                            <p className="font-italic mb-0 text-white">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.</p>
-                                        </Card.Body>
-                                    </Accordion.Collapse>
-                                </Card>
-                            </Accordion>
-                        </Col>
-
-                        <Col lg={6} className="mb-5">
-                            <Accordion defaultActiveKey="0">
-                                <Card bg="danger">
-                                    <Accordion.Toggle as={Card.Header} eventKey="0" role="button" aria-expanded="true" className="btn btn-danger py-2 shadow-sm with-chevron">
-                                        <p className="d-flex align-items-center justify-content-between mb-0 px-3 py-2"><strong className="text-uppercase">Taller de Base de Datos</strong><i className="fa fa-angle-down"></i></p>
-                                    </Accordion.Toggle>
-                                    <Accordion.Collapse eventKey="0" className="shadow-sm">
-                                        <Card.Body>
-                                            <p className="font-italic mb-0 text-white">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.</p>
-                                        </Card.Body>
-                                    </Accordion.Collapse>
-                                </Card>
-                            </Accordion>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col lg={6} className="mb-5">
-                            <Accordion defaultActiveKey="0">
-                                <Card bg="warning">
-                                    <Accordion.Toggle as={Card.Header} eventKey="0" role="button" aria-expanded="true" className="btn btn-warning py-2 shadow-sm with-chevron">
-                                        <p className="d-flex align-items-center justify-content-between mb-0 px-3 py-2"><strong className="text-uppercase">Interpretación de Requisitos</strong><i className="fa fa-angle-down"></i></p>
-                                    </Accordion.Toggle>
-                                    <Accordion.Collapse eventKey="0" className="shadow-sm">
-                                        <Card.Body>
-                                            <p className="font-italic mb-0 text-white">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.</p>
-                                        </Card.Body>
-                                    </Accordion.Collapse>
-                                </Card>
-                            </Accordion>
-                        </Col>
-
-                        <Col lg={6} className="mb-5">
-                            <Accordion defaultActiveKey="0">
-                                <Card bg="info">
-                                    <Accordion.Toggle as={Card.Header} eventKey="0" role="button" aria-expanded="true" className="btn btn-info py-2 shadow-sm with-chevron">
-                                        <p className="d-flex align-items-center justify-content-between mb-0 px-3 py-2"><strong className="text-uppercase">Construcción de Elementos de Software Web 1</strong><i className="fa fa-angle-down"></i></p>
-                                    </Accordion.Toggle>
-                                    <Accordion.Collapse eventKey="0" className="shadow-sm">
-                                        <Card.Body>
-                                            <p className="font-italic mb-0 text-white">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.</p>
-                                        </Card.Body>
-                                    </Accordion.Collapse>
-                                </Card>
-                            </Accordion>
-                        </Col>
-
+                        {this.state.modulos.map(modulo => {
+                            let r = faker.random.arrayElement(['primary', 'danger', 'warning', 'info'])
+                            return <Col lg={6} className="mb-5">
+                                <Accordion defaultActiveKey="0">
+                                    <Card bg={r}>
+                                        <Accordion.Toggle as={Card.Header} eventKey="0" role="button" aria-expanded="true" className={`btn btn-${r} py-2 shadow-sm with-chevron`}>
+                                            <p className="d-flex align-items-center justify-content-between mb-0 px-3 py-2"><strong className="text-uppercase">{modulo.modulo}</strong><i className="fa fa-angle-down"></i></p>
+                                        </Accordion.Toggle>
+                                        <Accordion.Collapse eventKey="0" className="shadow-sm">
+                                            <Card.Body>
+                                                <p className="font-italic mb-0 text-white">{faker.lorem.paragraph()}</p>
+                                            </Card.Body>
+                                        </Accordion.Collapse>
+                                    </Card>
+                                </Accordion>
+                            </Col>
+                        })}
                     </Row>
                 </div>
             </>
--- src/components/user/ProfileData.jsx
+++ src/components/user/ProfileData.jsx
@@ -1,47 +1,72 @@
 import React, { Component } from 'react'
 import { Table } from 'react-bootstrap'
 import UsuarioContext from '../../context/UsuarioContext'
+import { API_MEDIATEC_APP } from '../../constantes'
 
 
 export default class ProfileData extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+
+        }
+    }
+
+    async componentDidMount() {
+        let userSesion = JSON.parse(localStorage.getItem("usuario"))
+        let response = await fetch(`${API_MEDIATEC_APP}actores/${userSesion.idUsuario}`)
+        let usuario = await response.json()
+        this.setState(usuario)
+    }
+
     render() {
         return (
             <div>
-                <UsuarioContext.Consumer>
-                    {user => <>
-                        {user !== undefined ?
-                            <Table striped bordered hover>
-                                <thead>
-                                    <tr>
-                                        <th colSpan={2}>Datos de usuario</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>ID Usuario</td>
-                                        <td>{user.idUsuario}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Correo</td>
-                                        <td>{user.apellido}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Nombre</td>
-                                        <td>{user.nombre}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Apellido</td>
-                                        <td>{user.apellido}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Nombre Usuario</td>
-                                        <td>{user.nombreUsuario}</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
-                            : <div></div>}
-                    </>}
-                </UsuarioContext.Consumer>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th colSpan={2}>Datos de usuario</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>ID Usuario</td>
+                            <td>{this.state.id}</td>
+                        </tr>
+                        <tr>
+                            <td>Documento</td>
+                            <td>{this.state.documento}</td>
+                        </tr>
+                        <tr>
+                            <td>Nombre(s)</td>
+                            <td>{this.state.nombres}</td>
+                        </tr>
+                        <tr>
+                            <td>Apellido(s)</td>
+                            <td>{this.state.apellidos}</td>
+                        </tr>
+                        <tr>
+                            <td>Correo</td>
+                            <td>{this.state.correo}</td>
+                        </tr>
+                        <tr>
+                            <td>Celular</td>
+                            <td>{this.state.telefono_celular}</td>
+                        </tr>
+                        <tr>
+                            <td>Tipo de documento</td>
+                            <td>{this.state.tipo_documento}</td>
+                        </tr>
+                        <tr>
+                            <td>Genero</td>
+                            <td>{this.state.genero}</td>
+                        </tr>
+                        <tr>
+                            <td>Fecha de nacimiento</td>
+                            <td>{this.state.fecha_nacimiento}</td>
+                        </tr>
+                    </tbody>
+                </Table>
             </div>
         )
     }
